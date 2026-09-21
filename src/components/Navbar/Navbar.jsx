948741bd42c2948e1dcd628bd9c2f9b3eb4cc622
Terrence Reinhardt <terrencereinhardt041004@gmail.com>
2026-09-21 19:21:55 +0700

    Update: Adjust Navbar Design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,96 @@
-import { NavLink } from "react-router";
+import { Link, NavLink, useLocation } from "react-router";
+
+import { useFavorites } from "../../context/FavoritesContext";
 
 import "./Navbar.css";
 
 function Navbar() {
+    const location = useLocation();
+    const { favorites } = useFavorites();
+
+    const isExploreActive =
+        location.pathname === "/explore" ||
+        location.pathname.startsWith("/artwork/");
+
+    const isArtistsActive =
+        location.pathname === "/artists" ||
+        location.pathname.startsWith("/artist/");
+
     return (
         <header className="navbar">
             <div className="container navbar-container">
 
-                <NavLink
+                <Link
                     to="/"
-                    className="navbar-brand"
+                    className="navbar-logo"
+                    aria-label="SkyArt home"
                 >
-                    ARTELIER
-                </NavLink>
+                    SKYART
+                </Link>
 
-                <nav className="navbar-links">
-                    <NavLink to="/explore">
+                <nav
+                    className="navbar-navigation"
+                    aria-label="Main navigation"
+                >
+                    <NavLink
+                        to="/"
+                        end
+                        className={({ isActive }) =>
+                            isActive
+                                ? "navbar-link active"
+                                : "navbar-link"
+                        }
+                    >
+                        Home
+                    </NavLink>
+
+                    <Link
+                        to="/explore"
+                        className={
+                            isExploreActive
+                                ? "navbar-link active"
+                                : "navbar-link"
+                        }
+                    >
                         Explore
-                    </NavLink>
+                    </Link>
 
-                    <NavLink to="/artists">
+                    <Link
+                        to="/artists"
+                        className={
+                            isArtistsActive
+                                ? "navbar-link active"
+                                : "navbar-link"
+                        }
+                    >
                         Artists
-                    </NavLink>
+                    </Link>
 
-                    <NavLink to="/favorites">
+                    <NavLink
+                        to="/favorites"
+                        className={({ isActive }) =>
+                            isActive
+                                ? "navbar-link active"
+                                : "navbar-link"
+                        }
+                    >
                         Favorites
+
+                        {favorites.length > 0 && (
+                            <span className="navbar-favorites-count">
+                                {favorites.length}
+                            </span>
+                        )}
                     </NavLink>
 
-                    <NavLink to="/about">
+                    <NavLink
+                        to="/about"
+                        className={({ isActive }) =>
+                            isActive
+                                ? "navbar-link active"
+                                : "navbar-link"
+                        }
+                    >
                         About
                     </NavLink>
                 </nav>
